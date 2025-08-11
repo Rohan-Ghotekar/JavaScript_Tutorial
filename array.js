22fe5195586104e07f629deed1d0454f1d9462a3
Rohan-Ghotekar <rohanghotekar@outlook.com>
2025-08-11 12:37:27 +0000

    test

---
+++ array.js
@@ -0,0 +1,11 @@
+const array1=[1,2,3,4,5];
+const array2=[6,7,8,9,10];
+// array1.push(array2);
+// const newarray=array1.concat(array2);
+// console.log(newarray);
+// spread operator
+const newa=[...array1 , ...array2];
+console.log(newa);
+// // array1.shift();
+// array1.splice(1,3,13)
+// console.log(array1)
