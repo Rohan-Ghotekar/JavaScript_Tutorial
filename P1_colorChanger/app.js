56d04c3b45a4749b6ee2bc59556a8d42a6fd31a3
Rohan-Ghotekar <rohanghotekar@outlook.com>
2025-08-17 13:25:30 +0000

    project 1 and 2

---
+++ P1_colorChanger/app.js
@@ -0,0 +1,16 @@
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelector('body');
+
+buttons.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        if(e.target.id==="grey"){
+            body.style.backgroundColor=e.target.id;
+        }else if(e.target.id==="white"){
+            body.style.backgroundColor=e.target.id;
+        }else if(e.target.id==="yellow"){
+            body.style.backgroundColor=e.target.id;
+        }else{
+            body.style.backgroundColor=e.target.id;
+        }
+    })
+});
